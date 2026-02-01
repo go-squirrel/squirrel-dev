93b18eb2351ab6d03a696a7423dccdce4802b175
laisxn <191120844@qq.com>
2026-02-01 14:00:02 +0800

    refactor: apiserver auth use hash

--- internal/squ-apiserver/repository/auth/user.go
+++ internal/squ-apiserver/repository/auth/user.go
@@ -1,13 +1,16 @@
 package auth
 
-import "squirrel-dev/internal/squ-apiserver/model"
+import (
+	"squirrel-dev/internal/squ-apiserver/model"
+	"squirrel-dev/pkg/hash"
+)
 
 func (c *Client) Get(username, password string) bool {
 	var user model.User
-	err := c.DB.First(&user).Where("username = ? AND password = ?", username, password).Error
+	err := c.DB.Where("username = ?", username).First(&user).Error
 	if err != nil {
 		return false
 	}
-	return true
-
+	err = hash.ComparePassword(user.Password, password)
+	return err == nil
 }
